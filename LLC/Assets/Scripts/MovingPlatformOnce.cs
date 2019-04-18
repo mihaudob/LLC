7dab48a86b6ca5265d187f9848f7e3de433c41dd
mihaudob <mdobiecki97@gmail.com>
2019-04-18 13:49:57 +0200

    Ruchowe elementy
    Dodanie skryptow na ruch

---
+++ LLC/Assets/Scripts/MovingPlatformOnce.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatformOnce : MonoBehaviour
+{
+    public GameObject platform;
+    public float moveSpeed;
+    public Transform currentPoint;
+    public Transform[] points;
+    public int pointSelection;
+    void Start()
+    {
+        currentPoint = points[pointSelection];
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        platform.transform.position = Vector3.MoveTowards(platform.transform.position, currentPoint.position, Time.deltaTime * moveSpeed);
+
+    }
+}
