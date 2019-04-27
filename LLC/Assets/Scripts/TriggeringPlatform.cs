215d28cec5b88aaacdc6179a09d7ea7500528224
mihaudob <mdobiecki97@gmail.com>
2019-04-27 17:21:46 +0200

    Working Trigger ale Movement nie działa

---
+++ LLC/Assets/Scripts/TriggeringPlatform.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggeringPlatform : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Weszedł");
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("Wyszed");
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("Siedzi");
+
+    }
+}
