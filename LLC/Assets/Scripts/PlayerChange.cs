dedd55e4441a40fe9a2d44b2c7c228edd89af5d0
mihaudob <mdobiecki97@gmail.com>
2019-05-27 21:27:08 +0200

    Wielopostać i Multi Idealne

--- LLC/Assets/Scripts/PlayerChange.cs
+++ LLC/Assets/Scripts/PlayerChange.cs
@@ -6,18 +6,24 @@ public class PlayerChange : MonoBehaviour
 {
     public KeyCode changePlayers;
     public StaticFromMenu staticVariables;
-    public string thiCharacterName = "First";
+    public string thisCharacterName = "First";
     public int characterNumber = 3;
+    public int mageGravity = 2;
+    public int maceManGravity = 2;
+    public int ghostGravity = 2;
+    public float howHighPutCamera = 1;
+    public float howHighPutRest = 1;
     public GameObject mage;
     public GameObject ghost;
     public GameObject maceMan;
+    public GameObject playerPosition;
 
     int characterSelected = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(thiCharacterName == "First")
+        if(thisCharacterName == "First")
         {
             characterSelected = staticVariables.GetPlayerAChoice();
 
@@ -40,6 +46,10 @@ public class PlayerChange : MonoBehaviour
         }
         if(characterSelected == 1)
         {
+            mage.GetComponent<BoxCollider2D>().enabled = true;
+            ghost.GetComponent<BoxCollider2D>().enabled = false;
+            maceMan.GetComponent<BoxCollider2D>().enabled = false;
+
             mage.GetComponent<SpriteRenderer>().enabled = true;
             ghost.GetComponent<SpriteRenderer>().enabled = false;
             maceMan.GetComponent<SpriteRenderer>().enabled = false;
@@ -48,12 +58,25 @@ public class PlayerChange : MonoBehaviour
             ghost.GetComponent<Player>().enabled = false;
             maceMan.GetComponent<Player>().enabled = false;
 
+            playerPosition.GetComponent<Transform>().position = new Vector2(mage.GetComponent<Transform>().position.x, mage.GetComponent<Transform>().position.y + howHighPutCamera);
+            maceMan.GetComponent<Transform>().position = new Vector2(mage.GetComponent<Transform>().position.x, mage.GetComponent<Transform>().position.y + howHighPutRest/2);
+            ghost.GetComponent<Transform>().position = new Vector2(mage.GetComponent<Transform>().position.x, mage.GetComponent<Transform>().position.y + howHighPutRest/2);
+
             mage.GetComponent<Animator>().enabled = true;
             ghost.GetComponent<Animator>().enabled = false;
             maceMan.GetComponent<Animator>().enabled = false;
+
+            maceMan.GetComponent<Rigidbody2D>().gravityScale = 0;
+            ghost.GetComponent<Rigidbody2D>().gravityScale = 0;
+            mage.GetComponent<Rigidbody2D>().gravityScale = mageGravity;
         }
         if (characterSelected == 2)
         {
+
+            mage.GetComponent<BoxCollider2D>().enabled = false;
+            ghost.GetComponent<BoxCollider2D>().enabled = true;
+            maceMan.GetComponent<BoxCollider2D>().enabled = false;
+
             mage.GetComponent<SpriteRenderer>().enabled = false;
             ghost.GetComponent<SpriteRenderer>().enabled = true;
             maceMan.GetComponent<SpriteRenderer>().enabled = false;
@@ -62,12 +85,26 @@ public class PlayerChange : MonoBehaviour
             ghost.GetComponent<Player>().enabled = true;
             maceMan.GetComponent<Player>().enabled = false;
 
+            playerPosition.GetComponent<Transform>().position = new Vector2(ghost.GetComponent<Transform>().position.x, ghost.GetComponent<Transform>().position.y + howHighPutCamera);
+            mage.GetComponent<Transform>().position = new Vector2(ghost.GetComponent<Transform>().position.x, ghost.GetComponent<Transform>().position.y + howHighPutRest/2);
+            maceMan.GetComponent<Transform>().position = new Vector2(ghost.GetComponent<Transform>().position.x, ghost.GetComponent<Transform>().position.y + howHighPutRest/2);
+
             mage.GetComponent<Animator>().enabled = false;
             ghost.GetComponent<Animator>().enabled = true;
             maceMan.GetComponent<Animator>().enabled = false;
+
+            maceMan.GetComponent<Rigidbody2D>().gravityScale = 0;
+            mage.GetComponent<Rigidbody2D>().gravityScale = 0;
+            ghost.GetComponent<Rigidbody2D>().gravityScale = ghostGravity;
         }
         if (characterSelected == 3)
         {
+
+
+            mage.GetComponent<BoxCollider2D>().enabled = false;
+            ghost.GetComponent<BoxCollider2D>().enabled = false;
+            maceMan.GetComponent<BoxCollider2D>().enabled = true;
+
             mage.GetComponent<SpriteRenderer>().enabled = false;
             ghost.GetComponent<SpriteRenderer>().enabled = false;
             maceMan.GetComponent<SpriteRenderer>().enabled = true;
@@ -76,9 +113,17 @@ public class PlayerChange : MonoBehaviour
             ghost.GetComponent<Player>().enabled = false;
             maceMan.GetComponent<Player>().enabled = true;
 
+            playerPosition.GetComponent<Transform>().position = new Vector2(maceMan.GetComponent<Transform>().position.x, maceMan.GetComponent<Transform>().position.y + howHighPutCamera);
+            ghost.GetComponent<Transform>().position = new Vector2(maceMan.GetComponent<Transform>().position.x, maceMan.GetComponent<Transform>().position.y + howHighPutRest/2);
+            mage.GetComponent<Transform>().position = new Vector2(maceMan.GetComponent<Transform>().position.x, maceMan.GetComponent<Transform>().position.y + howHighPutRest/2);
+
             mage.GetComponent<Animator>().enabled = false;
             ghost.GetComponent<Animator>().enabled = false;
             maceMan.GetComponent<Animator>().enabled = true;
+            
+            mage.GetComponent<Rigidbody2D>().gravityScale = 0;
+            ghost.GetComponent<Rigidbody2D>().gravityScale = 0;
+            maceMan.GetComponent<Rigidbody2D>().gravityScale = maceManGravity;
 
         }
     }
